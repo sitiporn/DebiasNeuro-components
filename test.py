6a78df0d44004e4fbfa18460d01fd0097d21f214
sitiporn <sit.mte@gmail.com>
2022-12-23 13:53:59 +0700

    feat: treatment thesholds

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import json
 from utils import get_overlap_score
+import numpy as np
 
 data_path = '../debias_fork_clean/debias_nlu_clean/data/nli/'
 json_file = 'multinli_1.0_train.jsonl'
@@ -20,3 +21,35 @@ for i in range(len(df)):
 df['pair_label'] = pair_and_label
 
 df['overlap_scores'] = df['pair_label'].apply(get_overlap_score)
+
+# Todo: find percentile of entailment class
+
+
+"""
+def get_threshold(overlap_score, labels):
+
+    if labels == "entailment":
+
+        if overlap_score >= th:
+           return "treatment"
+        elif overlap_score <= 50:
+            return "no-treatment"
+
+        else:
+            return "none"
+
+    else:
+        return "none"
+"""
+
+# Todo: get overlap_score for whole entailment sets
+entail_mask = (df['gold_label'] == "entailment").tolist()
+overlap_scores = df['overlap_scores'][entail_mask]
+
+thresholds = {"treatment": None, "no-treatment": None}
+thresholds["no-treatment"] = np.percentile(overlap_scores,50)
+thresholds["treatment"] = np.percentile(overlap_scores,80)
+
+breakpoint()
+
+# do-treatment
