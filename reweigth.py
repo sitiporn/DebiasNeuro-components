8880809466bf35f2464b0e5b69578630f3ae47cf
sitiporn <sit.mte@gmail.com>
2023-05-22 10:05:45 +0700

    feat: reweight set

--- reweigth.py
+++ reweigth.py
@@ -1,6 +1,16 @@
 import os
 import os.path
 import pandas as pd
+import pickle
+import json
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import yaml
+from data import ExperimentDataset, Dev, get_predictions, print_config
+
+
 
 # 3 class {entailment: 0, contrandiction: 1, neatral: 2}
 def relabel(label):
@@ -21,38 +31,85 @@ def give_weight(label, probs):
 
     return 1 / probs
 
+with open("config.yaml", "r") as yamlfile:
+    config = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
 dev_path = "../debias_fork_clean/debias_nlu_clean/data/nli/"
 
 file = 'dev_prob_korn_lr_overlapping_sample_weight_3class.jsonl'
 
-result_path = os.path.join(os.path.join(dev_path, file))
-
-#Index(['gold_label', 'sentence1', 'sentence2', 'bias_probs'], dtype='object')
-df = pd.read_json(result_path, lines=True)
+dev_path = os.path.join(os.path.join(dev_path, file))
 
 
-# df['prob_score'] = df.apply(lambda x: [1, 2], axis=1)
-df['weight_score'] = df[['gold_label', 'bias_probs']].apply(lambda x: give_weight(*x), axis=1)
+
+tokenizer = AutoTokenizer.from_pretrained(config['model_name'])
+model = AutoModelForSequenceClassification.from_pretrained(config["model_name"])
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(DEVICE)
 
 avg_losses = []
 
+class ReweightDataset:
+
+    def __init__(self, dev_path, encode, DEBUG=False) -> None: 
+        
+        # combine these two set
+        self.encode = encode
+        self.df = pd.read_json(dev_path, lines=True)
+        
+        #Index(['gold_label', 'sentence1', 'sentence2', 'bias_probs'], dtype='object')
+        self.df['weight_score'] = self.df[['gold_label', 'bias_probs']].apply(lambda x: give_weight(*x), axis=1)
+        
+        self.inputs = {}
+
+        for  df_col in list(self.df.keys()): 
+            
+            self.inputs[df_col] = self.df[df_col].tolist()
+
+    def __len__(self):
+
+        return self.df.shape[0]
+    
+    def __getitem__(self, idx):
+
+        gold_label = self.inputs['gold_label'][idx]
+        premise = self.inputs['sentence1'][idx]
+        hypo = self.inputs['sentence2'][idx]
+        bias_prob = self.inputs['bias_probs'][idx]
+        weight_score = self.inputs['weight_score'][idx]
+
+        return gold_label, premise, hypo, bias_prob, weight_score
+
+reweighting_set = ReweightDataset(dev_path = dev_path, encode = tokenizer)
+reweighting_loader = DataLoader(reweighting_set, batch_size = 32, shuffle = False, num_workers=0)
+
 # quantifying by using losses
-for index, row in df.iterrows():
+# for index, row in df.iterrows():
 
-    gold_label = row['gold_label'] 
-    bias_prob = row['bias_probs']
-    weight_score = row['weight_score']
+#     gold_label = row['gold_label'] 
+#     bias_prob = row['bias_probs']
+#     weight_score = row['weight_score']
 
-    print(f"gold_label : {gold_label}, bias_prob : {bias_prob}, weight score : {weight_score}")
+#     print(f"gold_label : {gold_label}, bias_prob : {bias_prob}, weight score : {weight_score}")
     
     # using total loss to quantifying the impact 
     # 1. reweight probs from bias model
     # 2. aggregate with total loss
     # 3. using loss to do hyper parameter search for weaken rate with fixed masking rates
     # where is loss ? 
-
+    # focal loss ? 
+    # weight * cross entropy loss; weighted term aggregrate with cross entropy loss of main model
     # breakpoint()
+
+    # step bias only model
+    # 1. preparing handcarf feature (bias feature)
+    # 2. training bias model 
+    # 3. using bias model to quanifying the importance of each instance
+
+    # bias-only model; debias technique by reweight of the importance of instances towards losses of main model once traing mode in competiive methods
+    # mitigate paper dont reweighting just training bias-only model using as counterfactual 
+
+
 
 
 # Todo: get label maps 
