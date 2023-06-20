28b6880e3365fca7068549e4e60f61f65b659f17
sitiporn <sit.mte@gmail.com>
2023-06-20 10:02:21 +0700

    feat: get hans base with no condition result

--- data.py
+++ data.py
@@ -1119,6 +1119,141 @@ def convert_text_to_answer_base(config, raw_distribution_path): #, text_answer_p
 
         print(f"saving text answer's bert predictions: {text_answer_path}")
 
+def get_hans_result(raw_distribution_path, config):
+
+    if config['to_text']: convert_text_to_answer_base(config, raw_distribution_path)
+
+    performance_path =  '/'.join(raw_distribution_path.split('/')[:-1])
+    text_answer_path =  os.path.join(performance_path, f'hans_text_answers.txt')
+    score_path =  os.path.join(performance_path, f'hans_scores.txt')
+
+    # path to read inferences of model
+    # os.path.join(os.path.join(prediction_path, epsilon_path),  text_answer_path)
+    # path to write scores
+    # result_path = None #os.path.join(os.path.join(eval_path, epsilon_path),  result_path)
+
+    tables = {}
+
+    fi = open(text_answer_path, "r")
+
+    first = True
+    guess_dict = {}
+
+    for line in fi:
+        if first:
+            first = False
+            continue
+        else:
+            parts = line.strip().split(",")
+            guess_dict[parts[0]] = format_label(parts[1])
+
+    # load from hans set up
+    fi = open("../hans/heuristics_evaluation_set.txt", "r")
+
+    correct_dict = {}
+    first = True
+
+    heuristic_list = []
+    subcase_list = []
+    template_list = []
+
+    for line in fi:
+        if first:
+            labels = line.strip().split("\t")
+            idIndex = labels.index("pairID")
+            first = False
+            continue
+        else:
+            parts = line.strip().split("\t")
+            this_line_dict = {}
+            for index, label in enumerate(labels):
+                if label == "pairID":
+                    continue
+                else:
+                    this_line_dict[label] = parts[index]
+            correct_dict[parts[idIndex]] = this_line_dict
+
+            if this_line_dict["heuristic"] not in heuristic_list:
+                heuristic_list.append(this_line_dict["heuristic"])
+            if this_line_dict["subcase"] not in subcase_list:
+                subcase_list.append(this_line_dict["subcase"])
+            if this_line_dict["template"] not in template_list:
+                template_list.append(this_line_dict["template"])
+
+    heuristic_ent_correct_count_dict = {}
+    subcase_correct_count_dict = {}
+    template_correct_count_dict = {}
+    heuristic_ent_incorrect_count_dict = {}
+    subcase_incorrect_count_dict = {}
+    template_incorrect_count_dict = {}
+    heuristic_nonent_correct_count_dict = {}
+    heuristic_nonent_incorrect_count_dict = {}
+
+    for heuristic in heuristic_list:
+        heuristic_ent_correct_count_dict[heuristic] = 0
+        heuristic_ent_incorrect_count_dict[heuristic] = 0
+        heuristic_nonent_correct_count_dict[heuristic] = 0 
+        heuristic_nonent_incorrect_count_dict[heuristic] = 0
+
+    for subcase in subcase_list:
+        subcase_correct_count_dict[subcase] = 0
+        subcase_incorrect_count_dict[subcase] = 0
+
+    for template in template_list:
+        template_correct_count_dict[template] = 0
+        template_incorrect_count_dict[template] = 0
+
+    for key in correct_dict:
+        traits = correct_dict[key]
+        heur = traits["heuristic"]
+        subcase = traits["subcase"]
+        template = traits["template"]
+
+        guess = guess_dict[key]
+        correct = traits["gold_label"]
+
+        if guess == correct:
+            if correct == "entailment":
+                heuristic_ent_correct_count_dict[heur] += 1
+            else:
+                heuristic_nonent_correct_count_dict[heur] += 1
+
+            subcase_correct_count_dict[subcase] += 1
+            template_correct_count_dict[template] += 1
+        else:
+            if correct == "entailment":
+                heuristic_ent_incorrect_count_dict[heur] += 1
+            else:
+                heuristic_nonent_incorrect_count_dict[heur] += 1
+            subcase_incorrect_count_dict[subcase] += 1
+            template_incorrect_count_dict[template] += 1
+
+    tables['correct']  = { 'entailed': heuristic_ent_correct_count_dict, 'non-entailed': heuristic_nonent_correct_count_dict}
+    tables['incorrect'] = { 'entailed': heuristic_ent_incorrect_count_dict,  'non-entailed': heuristic_nonent_incorrect_count_dict}
+
+    for cur_class in ['entailed','non-entailed']:
+
+        print(f"Heuristic  {cur_class} results:")
+
+        if cur_class not in config["evaluations"][group].keys():  config["evaluations"][group][cur_class] = {}
+
+        for heuristic in heuristic_list:
+
+            correct = tables['correct'][cur_class][heuristic]
+            incorrect = tables['incorrect'][cur_class][heuristic]
+
+            total = correct + incorrect
+            percent = correct * 1.0 / total
+            print(heuristic + ": " + str(percent))
+
+            config["evaluations"][group][cur_class][heuristic] = percent
+
+    
+    with open(score_path, 'wb') as handle: 
+        pickle.dump(config["evaluations"], handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print(f'saving evaluation predictoins into : {score_path}')
+
+
 
 
 
