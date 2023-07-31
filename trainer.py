7816b636a89885a889a16f7b1aaab46955a0279f
sitiporn <sit.mte@gmail.com>
2023-07-31 21:49:58 +0700

    feat(trainer): be able to train

--- trainer.py
+++ trainer.py
@@ -40,14 +40,9 @@ from transformers import TrainingArguments, Trainer
 from datasets import Dataset
 
 def tokenize_function(examples):
-    breakpoint()
+    return tokenizer(examples["sentence1"], examples["sentence2"], padding="max_length", truncation=True) 
 
-    # Todo: combine to list
-    # pair_sentences = [[premise, hypo] for premise, hypo in zip(cur_inputs['sentence1'], cur_inputs['sentence2'])]
-    # pair_sentences = tokenizer(pair_sentences, padding=True, truncation=True, return_tensors="pt")
-
-    return True #tokenizer(examples["text"], padding="max_length", truncation=True)
-
+def to_label_id(text_label): return label_maps[text_label]
 
 # class TrainingDataset(Dataset):
 #     def __init__(self, config, encode, DEBUG=False) -> None: 
@@ -56,51 +51,60 @@ def tokenize_function(examples):
 #         # self.dataset = [{'label': row['gold_label'], 'text': [ row['sentence1'], row['sentence2']] } for index, row in df.iterrows()] 
 
 def main():
+    global tokenizer
+    global label_maps
+    
     with open("train_config.yaml", "r") as yamlfile:
         # baseline config
         config = yaml.load(yamlfile, Loader=yaml.FullLoader)
     
+    output_dir = '../models/baseline/'
+    label_maps = {"entailment": 0, "contradiction": 1, "neutral": 2}
     tokenizer = AutoTokenizer.from_pretrained(config['dataset_reader']['tokenizer']['model_name'])
+    
     model = BertForSequenceClassification.from_pretrained(config['model']['tokens']["model_name"], num_labels=3 )
     df = pd.read_json(os.path.join(config['data_path'], config['train_data']), lines=True)
     df_new = df[['sentence1', 'sentence2', 'gold_label']]
+    df_new.rename(columns = {'gold_label':'label'}, inplace = True)
+    df_new['label'] = df_new['label'].apply(lambda label_text: to_label_id(label_text))
+    
     train_set = Dataset.from_pandas(df_new)
+    tokenized_datasets = train_set.map(tokenize_function, batched=True)
+    
 
-    breakpoint()
 
-    # train_dataset = TrainingDataset(config=config, encode=tokenizer)
-
-    # with open("config.yaml", "r") as yamlfile:
-    #     config = yaml.load(yamlfile, Loader=yaml.FullLoader)
-    #     print(config)
+    # "trainer": {
+    #   "num_epochs": 3,
+    #   "validation_metric": "+accuracy",
+    #   "learning_rate_scheduler": {
+    #     "type": "slanted_triangular",
+    #     "cut_frac": 0.06
+    #   },
+    #   "optimizer": {
+    #     "type": "huggingface_adamw",
+    #     "lr": 5e-5,
+    #     "weight_decay": 0.1,
+    #   },
+    #   "use_amp": true,
+    #   "cuda_device" : 0,
+    # }
     
     # training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
+    training_args = TrainingArguments(output_dir = output_dir,
+                                      learning_rate = 5e-5,
+                                      weight_decay = 0.1,
+                                      per_device_train_batch_size = 32,
+                                      num_train_epochs = 3,
+                                      half_precision_backend = 'amp')
     
-    # tokenizer = AutoTokenizer.from_pretrained(config['model_name'])
-    # model = BertForSequenceClassification.from_pretrained(config["model_name"])
     
-    breakpoint()
-    
-    # experiment_set = ExperimentDataset(config, encode = tokenizer)                            
-    # dataloader = DataLoader(experiment_set, batch_size = 32, shuffle = False, num_workers=0)
-    
-    # trainer = Trainer(
-    #     model,
-    #     training_args,
-    #     train_dataset= dataloader,
-    #     tokenizer=tokenizer,)
-
-
-    # trainer.train()
-    
-    # trainer = Trainer(
-    #     model,
-    #     training_args,
-    #     train_dataset=tokenized_datasets["train"],
-    #     eval_dataset=tokenized_datasets["validation"],
-    #     data_collator=data_collator,
-    #     tokenizer=tokenizer,)
+    trainer = Trainer(
+        model,
+        training_args,
+        train_dataset=tokenized_datasets,
+        tokenizer=tokenizer,)
         
+    breakpoint()
 
 
 if __name__ == "__main__":
