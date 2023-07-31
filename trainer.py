e93a95697e875c1be5fc397ecf1f918285f4569d
sitiporn <sit.mte@gmail.com>
2023-07-31 15:52:48 +0700

    feat(trainer): add huggingface-style training set

--- trainer.py
+++ trainer.py
@@ -35,19 +35,72 @@ from data import get_analysis
 from transformers import AutoTokenizer, BertForSequenceClassification
 from data import exclude_grad
 from transformers import Trainer
+# from torch.utils.data import Dataset, DataLoader
+from transformers import TrainingArguments, Trainer
 from datasets import Dataset
+
+def tokenize_function(examples):
+    breakpoint()
+
+    # Todo: combine to list
+    # pair_sentences = [[premise, hypo] for premise, hypo in zip(cur_inputs['sentence1'], cur_inputs['sentence2'])]
+    # pair_sentences = tokenizer(pair_sentences, padding=True, truncation=True, return_tensors="pt")
+
+    return True #tokenizer(examples["text"], padding="max_length", truncation=True)
+
+
+# class TrainingDataset(Dataset):
+#     def __init__(self, config, encode, DEBUG=False) -> None: 
+#         df = pd.read_json(os.path.join(config['data_path'], config['train_data']), lines=True)
+#         dataset = df[['sentence1', 'sentence2', 'gold_label']].to_dict()
+#         # self.dataset = [{'label': row['gold_label'], 'text': [ row['sentence1'], row['sentence2']] } for index, row in df.iterrows()] 
 
 def main():
     with open("train_config.yaml", "r") as yamlfile:
         # baseline config
         config = yaml.load(yamlfile, Loader=yaml.FullLoader)
-
-
+    
     tokenizer = AutoTokenizer.from_pretrained(config['dataset_reader']['tokenizer']['model_name'])
     model = BertForSequenceClassification.from_pretrained(config['model']['tokens']["model_name"], num_labels=3 )
     df = pd.read_json(os.path.join(config['data_path'], config['train_data']), lines=True)
+    df_new = df[['sentence1', 'sentence2', 'gold_label']]
+    train_set = Dataset.from_pandas(df_new)
 
+    breakpoint()
+
+    # train_dataset = TrainingDataset(config=config, encode=tokenizer)
+
+    # with open("config.yaml", "r") as yamlfile:
+    #     config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    #     print(config)
     
+    # training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
+    
+    # tokenizer = AutoTokenizer.from_pretrained(config['model_name'])
+    # model = BertForSequenceClassification.from_pretrained(config["model_name"])
+    
+    breakpoint()
+    
+    # experiment_set = ExperimentDataset(config, encode = tokenizer)                            
+    # dataloader = DataLoader(experiment_set, batch_size = 32, shuffle = False, num_workers=0)
+    
+    # trainer = Trainer(
+    #     model,
+    #     training_args,
+    #     train_dataset= dataloader,
+    #     tokenizer=tokenizer,)
+
+
+    # trainer.train()
+    
+    # trainer = Trainer(
+    #     model,
+    #     training_args,
+    #     train_dataset=tokenized_datasets["train"],
+    #     eval_dataset=tokenized_datasets["validation"],
+    #     data_collator=data_collator,
+    #     tokenizer=tokenizer,)
+        
 
 
 if __name__ == "__main__":
