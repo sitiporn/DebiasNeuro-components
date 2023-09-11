a1c9aaab2a4cfd643d4f5ee044038f119d13f5a3
sitiporn <sit.mte@gmail.com>
2023-09-11 14:15:15 +0700

    feat(trainer): add custom label smoother

--- trainer_pt_utils.py
+++ trainer_pt_utils.py
@@ -7,6 +7,9 @@ from torchnlp.samplers.sorted_sampler import SortedSampler
 from typing import Iterator, Iterable, Optional, Sequence, List, TypeVar, Generic, Sized, Union
 from transformers.tokenization_utils_base import BatchEncoding
 from transformers.utils import logging
+from torch import nn
+from dataclasses import dataclass
+
 logger = logging.get_logger(__name__)
 
 def identity(x):
@@ -306,14 +309,62 @@ class LengthGroupedSampler(Sampler):
 
     def __iter__(self):
         indices = get_length_grouped_indices(self.lengths, self.batch_size, generator=self.generator)
+        breakpoint()
         return iter(indices)
+
+# Todo: add loss compute loss
+# custom smoothed_loss
+@dataclass
+class CustomLabelSmoother:
+    """
+    Adds label-smoothing on a pre-computed output from a Transformers model.
+    Background:
+        follow: https://github.com/huggingface/transformers/blob/v4.33.0/src/transformers/trainer_pt_utils.py#L27
+    Args:
+        epsilon (`float`, *optional*, defaults to 0.1):
+            The label smoothing factor.
+        ignore_index (`int`, *optional*, defaults to -100):
+            The index in the labels to ignore when computing the loss.
+    """
+
+    epsilon: float = 0.1
+    ignore_index: int = -100
+
+    def __call__(self, model_output, labels, shift_labels=False):
+        logits = model_output["logits"] if isinstance(model_output, dict) else model_output[0]
+        if shift_labels:
+            logits = logits[..., :-1, :].contiguous()
+            labels = labels[..., 1:].contiguous()
+
+        log_probs = -nn.functional.log_softmax(logits, dim=-1)
+        if labels.dim() == log_probs.dim() - 1:
+            labels = labels.unsqueeze(-1)
+
+        padding_mask = labels.eq(self.ignore_index)
+        # In case the ignore_index is -100, the gather will fail, so we replace labels by 0. The padding_mask
+        # will ignore them in any case.
+        labels = torch.clamp(labels, min=0)
+        nll_loss = log_probs.gather(dim=-1, index=labels)
+        # works for fp16 input tensor too, by internally upcasting it to fp32
+        smoothed_loss = log_probs.sum(dim=-1, keepdim=True, dtype=torch.float32)
+
+        nll_loss.masked_fill_(padding_mask, 0.0)
+        smoothed_loss.masked_fill_(padding_mask, 0.0)
+
+        # Take the mean over the label dimensions, then divide by the number of active elements (i.e. not-padded):
+        num_active_elements = padding_mask.numel() - padding_mask.long().sum()
+        nll_loss = nll_loss.sum() / num_active_elements
+        smoothed_loss = smoothed_loss.sum() / (num_active_elements * log_probs.shape[-1])
+        print(f'custom label smoother')
+        return (1 - self.epsilon) * nll_loss + self.epsilon * smoothed_loss
+
 
 """
 Example
+"""
 sampler = SequentialSampler(range(10))
 print(f" BatchSampler:")
 print(list(BatchSampler(sampler, batch_size=3, drop_last=False)))
 print(f" BucketBatchSampler:")
 print(list(BucketBatchSampler(sampler, batch_size=3, drop_last=False)))
 breakpoint()
-"""
