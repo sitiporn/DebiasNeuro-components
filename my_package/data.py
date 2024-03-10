d43f1b2fb02c2ddca714010d5b828134548d6287
sitiporn <sit.mte@gmail.com>
2024-03-10 13:55:14 +0700

    feat(get_all_checkpoints):  add get checkpoints

--- my_package/data.py
+++ my_package/data.py
@@ -914,6 +914,22 @@ def get_all_model_paths(LOAD_MODEL_PATH):
     # assert len(clean_model_files) == num_seeds, f"is not {num_seeds} runs"
     return {path.split('/')[3].split('_')[-1]: path for path in clean_model_files}
     
+def get_all_checkpoints(output_dir):
+    import pathlib
+    model_files = pathlib.Path(output_dir)
+    model_files.rglob('*.bin')
+    all_model_files = {} 
+    checkpoint_paths = []
+    
+    for f in model_files.rglob("*.bin"):
+        if 'pytorch_model' in str(f): 
+            checkpoint_paths.append(str(f))
+
+    return checkpoint_paths
+        
+    
+
+
 def eval_model(model, config, tokenizer, DEVICE, LOAD_MODEL_PATH, method_name, is_load_model=True, is_optimized_set = False):
     """ to get predictions and score on test and challenge sets"""
     distributions = {}
